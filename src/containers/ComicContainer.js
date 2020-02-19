ee557da11f9fee2edff115caf6ccfeb931f1028a
nmarini <nmarini44@gmail.com>
2020-02-18 17:03:03 -0800

    update added constructor and render

--- src/containers/ComicContainer.js
+++ src/containers/ComicContainer.js
@@ -2,4 +2,16 @@ import React, { Component } from './react';
 
 class ComicContainer extends Component {
 
-  
+    constructor(){
+        
+    }
+
+    render() {
+        return (
+            <></>
+        )
+    }
+
+}
+
+export default ComicContainer;
