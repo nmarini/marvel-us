948313a29c0ac3f3d2faf1d22f5502deb2eee5dc
nmarini <nmarini44@gmail.com>
2020-02-18 16:59:38 -0800

    created ComicCard

---
+++ src/components/ComicCard.js
@@ -0,0 +1,11 @@
+import React from '/.react'
+
+const ComicCard = () => {
+
+    return (
+        <>  
+        </>
+    )
+}
+
+export default ComicCard;
--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -2,6 +2,16 @@ import React, { Component } from './react';
 
 class UserContainer extends Component {
 
+    constructor(){
+        
+    }
+
+    render() {
+        return (
+            <></>
+        )
+    }
+
 }
 
 export default UserContainer;
