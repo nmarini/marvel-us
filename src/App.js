627c4ffd4c258e4a34b329ce5488e6c21399fb79
Nick Marini <nmarini44@gmail.com>
2020-02-25 17:09:45 -0500

    updated App css for wrapper and app classnames

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      App
-    </div>
-  );
+class App extends Component {
+
+
+  render() {
+    return (
+      <div className="App-wrapper">
+        <header className="App">
+          {/* add logo image */}
+          {/* <img src={} className="App-logo" alt="logo" /> */}
+          <p>
+            {/* First Introduction of Application */}
+            Marvel Us
+          </p>
+
+        </header>
+      </div>
+    )
+  }
+  
 }
 
 export default App;
