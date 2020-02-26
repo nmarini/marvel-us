43225d2ffe654b6b8ae32aad2ce552f8ae6c6e5f
Nick Marini <nmarini44@gmail.com>
2020-02-25 19:57:24 -0500

    added title and description to ComicCard component

--- src/components/ComicCard.js
+++ src/components/ComicCard.js
@@ -1,10 +1,11 @@
 import React from '/.react'
 
-const ComicCard = () => {
+const ComicCard = ({comic}) => {
 
     return (
         <>
-            ComicCard  
+            <h1>{ComicCard.title}</h1>  
+            Description:{comic.description ? <p>{comic.description}</p> : "Sorry...no descriptionn available"}
         </>
     )
 }
